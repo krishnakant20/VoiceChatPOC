1972f9454dbf965443abdbcd53f71a454201ffd9
krishnakant20 <89294839+krishnakant20@users.noreply.github.com>
2026-07-02 09:36:23 +0530

    voice btn ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import {
   Ionicons,
   MaterialCommunityIcons,
 } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 import {
   ExpoSpeechRecognitionModule,
   useSpeechRecognitionEvent,
@@ -16,6 +17,13 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
 
 export default function HomeScreen() {
   const [messages, setMessages] = useState<
@@ -69,27 +77,43 @@ export default function HomeScreen() {
     });
   };
 
+  // orb
+  const scale = useSharedValue(1);
+
+  useEffect(() => {
+    scale.value = withRepeat(
+      withSequence(
+        withTiming(1.08, { duration: 1200 }),
+        withTiming(0.94, { duration: 1200 })
+      ),
+      -1,
+      true
+    );
+  }, []);
+
+  const orbStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
+
   return (
     <View style={styles.container}>
-      {/* <View style={styles.header}>
-        <Text style={styles.headerTitle}>Voice Chat</Text>
-      </View> */}
-
       <View style={styles.header}>
         <TouchableOpacity style={styles.circleBtn}>
-          <Feather name="menu" size={34} color="#fff" />
+          <Feather name="menu" size={34} color="#111" />
         </TouchableOpacity>
 
         <Text style={styles.headerTitle}>
-          <Text style={{ color: "#fff", fontWeight: "700" }}>ChatGPT</Text>
-          <Text style={{ color: "#9ca3af" }}> Voice</Text>
+          <Text style={{ color: "#111", fontWeight: "700" }}>
+            ChatGPT
+          </Text>
+          <Text style={{ color: "#555" }}> Voice</Text>
         </Text>
 
         <TouchableOpacity style={styles.circleBtn}>
           <Ionicons
             name="ellipsis-horizontal"
             size={30}
-            color="#fff"
+            color="#111"
           />
         </TouchableOpacity>
       </View>
@@ -124,7 +148,7 @@ export default function HomeScreen() {
         )}
       />
 
-      <TouchableOpacity
+      {/* <TouchableOpacity
         style={[
           styles.micButton,
           { backgroundColor: isListening ? "#EF4444" : "#111827" },
@@ -133,11 +157,27 @@ export default function HomeScreen() {
         <Text style={styles.micText}>
           {isListening ? "🎙️" : "🎤"}
         </Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
+      <View style={styles.voiceOrb}>
+        <View style={styles.voiceGradient} />
+        <Animated.View style={[styles.voiceOrb, orbStyle]}>
+          <LinearGradient
+            colors={[
+              "#F9FAF6",
+              "#D9F4FF",
+              "#77C8FF",
+              "#1976FF",
+            ]}
+            start={{ x: 0.2, y: 0 }}
+            end={{ x: 0.8, y: 1 }}
+            style={styles.voiceGradient}
+          />
+        </Animated.View>
+      </View>
       <View style={styles.bottomContainer}>
         <View style={styles.inputBox}>
           <TouchableOpacity>
-            <Feather name="plus" size={32} color="#fff" />
+            <Feather name="plus" size={34} color="#111" />
           </TouchableOpacity>
 
           <Text style={styles.placeholder}>Type</Text>
@@ -149,14 +189,17 @@ export default function HomeScreen() {
         >
           <MaterialCommunityIcons
             name="microphone-outline"
-            size={34}
-            color="#fff"
+            size={36}
+            color="#111"
           />
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.closeButton}>
-          <Ionicons name="close" size={42} color="#000" />
-        </TouchableOpacity>
+          <Ionicons
+            name="close"
+            size={42}
+            color="#fff"
+          />        </TouchableOpacity>
       </View>
     </View>
   );
@@ -203,18 +246,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "600",
   },
-  // header: {
-  //   height: 70,
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   borderBottomWidth: 1,
-  //   borderBottomColor: "#ddd",
-  // },
-
-  // headerTitle: {
-  //   fontSize: 22,
-  //   fontWeight: "bold",
-  // },
   bubble: {
     padding: 12,
     borderRadius: 12,
@@ -238,47 +269,52 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
   },
-
   headerTitle: {
     flex: 1,
     marginLeft: 18,
-    fontSize: 24,
+    fontSize: 23,
+    fontWeight: "600",
   },
-
   circleBtn: {
-    width: 62,
-    height: 62,
-    borderRadius: 31,
-    backgroundColor: "#1f1f1f",
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: "#F6F6F6",
     justifyContent: "center",
     alignItems: "center",
-    borderWidth: 1,
-    borderColor: "#3b3b3b",
   },
-
   voiceOrb: {
     alignSelf: "center",
-    marginBottom: 35,
-  },
+    // marginBottom: 32,
+    position: "absolute",
+    bottom: 64,
 
+    // shadowColor: "#2F80ED",
+    // shadowOpacity: 0.35,
+    // shadowRadius: 30,
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 12,
+    // },
+  },
   voiceGradient: {
-    width: 120,
-    height: 120,
-    borderRadius: 60,
-    backgroundColor: "#3B82F6",
+    width: 118,
+    height: 118,
+    borderRadius: 59,
+    overflow: "hidden",
   },
 
   bottomContainer: {
     flexDirection: "row",
     alignItems: "center",
     marginHorizontal: 18,
-    marginBottom: 30,
+    marginBottom: 28,
   },
 
   inputBox: {
     flex: 1,
     height: 72,
-    backgroundColor: "#1f1f1f",
+    backgroundColor: "#F7F7F7",
     borderRadius: 36,
     flexDirection: "row",
     alignItems: "center",
@@ -286,26 +322,25 @@ const styles = StyleSheet.create({
   },
 
   placeholder: {
-    color: "#9ca3af",
+    color: "#888",
     fontSize: 20,
-    marginLeft: 18,
+    marginLeft: 16,
   },
 
   voiceButton: {
     width: 72,
     height: 72,
     borderRadius: 36,
-    backgroundColor: "#1f1f1f",
+    backgroundColor: "#F7F7F7",
     justifyContent: "center",
     alignItems: "center",
     marginLeft: 14,
   },
-
   closeButton: {
     width: 72,
     height: 72,
     borderRadius: 36,
-    backgroundColor: "#fff",
+    backgroundColor: "#111",
     justifyContent: "center",
     alignItems: "center",
     marginLeft: 14,
